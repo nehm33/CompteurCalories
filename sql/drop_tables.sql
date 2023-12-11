9d0ed9521970ac7ec4fd197b293b65fbc1bf8fa1
Emmanuel YAO <nehm33@gmail.com>
2023-12-11 12:02:37 +0100

    Add a file to delete sql tables

---
+++ sql/drop_tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE users;
+DROP TABLE aliments;
+DROP TABLE plats;
+DROP TABLE recettes;
+DROP TABLE composition_journal_aliment;
+DROP TABLE composition_journal_plat;
+DROP TABLE codeBarre;
